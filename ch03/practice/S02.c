a89245015faef266a79a7a268808b621552c8c70
edz <edz@TT.local>
2019-09-19 09:11:28 +0800

    second

--- ch03/practice/S02.c
+++ ch03/practice/S02.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 int main (int argc,char const *rgv[])
 {   int a,b;
 	printf("请输入：1.剪刀 2.石头 3.布\n");
 	scanf("%d",&a);
-	b=rand()%4;
+	srand((unsigned)time(NULL));
+	b=1+rand()%3;
 	switch(a){
 		case 1:printf("你出的是剪刀\n");break;
 		case 2:printf("你出的是石头\n");break;
